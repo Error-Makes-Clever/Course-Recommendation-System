f67c6447388bb30aef28f3d970b637d63a0d8887
Sujil S <sujil192110912@gmail.com>
2025-07-07 13:55:08 +0530

    Course Recommendation System

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ import backend as backend
 load_dotenv()
 url = os.getenv('SUPABASE_URL')
 key = os.getenv('SUPABASE_KEY')
+
+if not url or not key:
+    raise ValueError("Missing SUPABASE_URL or SUPABASE_KEY")
+    
 supabase = create_client(url, key)
 
 # Model list
